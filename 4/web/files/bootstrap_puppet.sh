f70b4cf96c8920ee5d78ddeb7c45db7bc2e3eb20
James Turnbull <james@lovedthanlost.net>
2018-06-19 01:25:33 +0200

    Updated script

--- 4/web/files/bootstrap_puppet.sh
+++ 4/web/files/bootstrap_puppet.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-sudo curl https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb -o puppetlabs-release.deb
-sudo dpkg -i puppetlabs-release.deb
+curl https://apt.puppetlabs.com/puppet-release-xenial.deb -o puppet-release-xenial.deb
+sleep 15
+sudo dpkg -i puppet-release-xenial.deb
 sudo apt-get -qq update
 sudo apt-get install -yq puppet-agent
 sudo /opt/puppetlabs/bin/puppet module install puppet-nginx
+sudo /opt/puppetlabs/bin/puppet module install puppetlabs-apt
 cat >/tmp/nginx.pp << "EOF"
 class{'nginx': }
-nginx::resource::vhost{'www.example.com':
+nginx::resource::server{'www.example.com':
     www_root => '/var/www/html/',
-  }
+}
 EOF
 sudo /opt/puppetlabs/bin/puppet apply /tmp/nginx.pp
